4ff3c8974a9dcf854a02f6ea4a6b7c0d5ebc615a
krishangguptafibonacciresearch <krishanggupta@fibonacciresearch.in>
2024-12-21 16:46:58 +0530

    Update periodic_runner_main.py

--- periodic_runner_main.py
+++ periodic_runner_main.py
@@ -74,7 +74,7 @@ def save_data(return_interval, IntradayObject, mysymboldict,Intraday_data_files,
 Intraday_data_files = "Intraday_data_files" # Read current dataset of historical data
 os.makedirs(Intraday_data_files, exist_ok=True)
 Daily_backup_files="Daily_backup_files"     # Store daily data for all tickers as backup
-os.makedirs(Daily_backup_files)
+os.makedirs(Daily_backup_files, exist_ok=True)
 os.makedirs('temp',exist_ok=True) # Temporary file to hold new Intraday data. Later gets renamed to "Intraday_data_files" after new and old data gets Merged
 
 
