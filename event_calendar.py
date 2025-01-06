0149744a565038afcf446fb375e07bd23c7ed2aa
krishangguptafibonacciresearch <krishanggupta@fibonacciresearch.in>
2025-01-06 13:44:06 +0530

    Update event_calendar.py

--- event_calendar.py
+++ event_calendar.py
@@ -275,6 +275,7 @@ def StoreCalendar(cal_df,**kwargs):
             elif index==1:
                 output_file_name=kwargs.get('output_file_name')
                 
+            df.to_excel(os.path.join(output_directory_name,f"{output_file_name}.xlsx"),index=False)
             merged_name,merged_df=MergeCalendar(df,output_file_name,**kwargs)
             merged_df.to_excel(os.path.join(output_directory_name,f"{merged_name}.xlsx"),index=False)
 
