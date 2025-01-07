00ce265a334a0e10785b4b544ddaf6897baa5a80
krishangguptafibonacciresearch <krishanggupta@fibonacciresearch.in>
2025-01-08 05:28:51 +0530

    Update returns_main.py

--- returns_main.py
+++ returns_main.py
@@ -4,7 +4,7 @@ from preprocessing import ManipulateTimezone
 from events import Events
 from returns import Returns
 from nonevents import Nonevents
-from periodic_runner_main import INTRADAY_FILES
+from periodic_runner_main import INTRADAY_FILES as Intraday_data_files
 import os
 
 
