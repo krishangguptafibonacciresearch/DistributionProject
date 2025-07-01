4e67622710637b87407027fac5035e27bf397936
krishangguptafibonacciresearch <krishanggupta@fibonacciresearch.in>
2025-07-02 00:15:40 +0530

    Update intradaydata.py
    1m has a default period of 1 month, YF only allows 8d

--- intradaydata.py
+++ intradaydata.py
@@ -57,7 +57,10 @@ class Intraday:
 
         elif self.start_intraday==-1 and self.end_intraday==-1:
             # Neither start nor end date is specified
-            data = yf.download(tickers=self.tickers, interval=self.interval)
+            if(self.interval =='1m'):
+                data = yf.download(tickers=self.tickers, interval=self.interval , period='7d')
+            else:
+                data = yf.download(tickers=self.tickers, interval=self.interval)
         # Return data for specific tickers as a dictionary 
         try:        
             if specific_tickers!=[]:
